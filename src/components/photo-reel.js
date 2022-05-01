8d482ccec2128eb52b7ddab2e33db364607a7c09
Madeline Undis <mundis@smith.edu>
2022-05-01 12:08:37 -0700

    upgrade gatsby 3

--- src/components/photo-reel.js
+++ src/components/photo-reel.js
@@ -16,12 +16,6 @@ const GatsbyImage = ({ className, children }) => (
   </a>
 );
 
-const Photo = styled(GatsbyImage)`
-.gatsby-image-wrapper > div[aria-hidden="true"] {
-  display: none;
-}
-`;
-
 
 const ChevronLeftIcon = ({ size = 20 }) => (
   <svg style={{ width: size, height: size }} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
